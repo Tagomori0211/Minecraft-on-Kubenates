4639008d7ef7da542dfff12cf2c4691dc0ffe0aa
Tagomori0211 <tagomoriyuukichi@gmail.com>
2026-04-18 01:28:27 +0900

    fix(infra): Add incoming UDP 19132 port for Bedrock Edition to GCP firewall

--- Terraform/gke.tf
+++ Terraform/gke.tf
@@ -80,11 +80,16 @@ resource "google_compute_firewall" "minecraft_tcp" {
     ports    = ["25565"]
   }
 
+  allow {
+    protocol = "udp"
+    ports    = ["19132"]
+  }
+
   source_ranges = ["0.0.0.0/0"]
 
   target_tags = ["minecraft"]
 
-  description = "Allow Minecraft TCP traffic"
+  description = "Allow Minecraft TCP/UDP traffic"
 }
 
 # 内部通信用（GKE 内部）
